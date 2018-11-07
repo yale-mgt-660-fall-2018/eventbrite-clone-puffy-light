e65d0e44c7c20c5038de0167ca44e8cbc8a8f59a
Kyle L. Jensen <kljensen@gmail.com>
2018-11-06 20:18:05 -0500

    Tweak SQL for people on c9

--- src/models/schema.sql
+++ src/models/schema.sql
@@ -1,11 +1,7 @@
--- Load up pycrypto so that we can do password hashing
-DROP EXTENSION IF EXISTS pgcrypto;
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
 DROP TABLE IF EXISTS events CASCADE;
 CREATE TABLE IF NOT EXISTS events (
     -- Integer primary key for events
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     -- 'date' is a reserved word in some SQL dialects.
     -- Here I quoted it to make sure it is interpreted
@@ -19,9 +15,4 @@ CREATE TABLE IF NOT EXISTS events (
     created_at TIMESTAMP WITH TIME ZONE
         NOT NULL
         DEFAULT current_timestamp
-
 );
--- Turn on verbose error messages, which helps our JavaScript
--- code handle database errors in a graceful manner.
-SET log_error_verbosity TO 'verbose';
--- \set VERBOSITY verbose
