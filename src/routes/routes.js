c0cf3f4d7f70ed717093616be5524f0123e21822
Ken Chanseau Saint-Germain <ken.chanseau@yahoo.fr>
2018-11-09 14:25:22 -0500

    Completed form with POST request

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router');
 const indexControllers = require('../controllers/index.js');
 const neweventControllers = require('../controllers/newevent.js');
+const eventcreatedControllers = require('../controllers/eventcreated.js');
 
 const router = new Router();
 router.get('/', indexControllers.index);
 router.get('/newevent.html', neweventControllers.newevent);
+router.post('/eventcreated.html', eventcreatedControllers.newevent);
 
 module.exports = router;
