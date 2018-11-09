d3a316f0234b5d96b278c6473f35a610d3e95145
Isaac Blasco <isaac.blascomir@yale.edu>
2018-11-08 19:00:58 -0500

    changes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,15 @@
  * @param  {Context} ctx - A Koa Context
  * @returns {Promise} - Returns a promise that resolves to undefined
  */
+const events = require('../models/events.js');
+
 async function index(ctx) {
     const template = 'index.njk';
-    const waysOfBeingAwesome = ['awesome1', 'awesome2', 'awesome3'];
-    return ctx.render(template, { waysOfBeingAwesome });
+    //const waysOfBeingAwesome = ['awesome1', 'awesome2', 'awesome3'];
+
+    const eventList = events.getAllEvents(ctx.db);
+
+    return ctx.render(template, { eventList });
 }
 
 module.exports = {
