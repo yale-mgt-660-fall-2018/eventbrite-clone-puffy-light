008dbaa694b96062eb4aae5fcff571f964fbd7a4
Ken Chanseau Saint-Germain <ken.chanseau@yahoo.fr>
2018-11-09 13:32:12 -0500

    Creating the new event page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 const Router = require('koa-router');
 const indexControllers = require('../controllers/index.js');
+const neweventControllers = require('../controllers/newevent.js');
 
 const router = new Router();
 router.get('/', indexControllers.index);
+router.get('/newevent.html', neweventControllers.newevent);
 
 module.exports = router;
