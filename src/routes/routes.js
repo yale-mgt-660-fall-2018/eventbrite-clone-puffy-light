1cf5810563c10290d09dfb650b2daeab9c381975
Ken Chanseau Saint-Germain <ken.chanseau@yahoo.fr>
2018-11-14 14:13:04 -0500

    Revert "Revert "routes for event page""
    This reverts commit dbbfb56e7ee0f06cb59d08dd8b2b1a6997dad449.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 const indexControllers = require('../controllers/index.js');
 const neweventControllers = require('../controllers/newevent.js');
 const eventcreatedControllers = require('../controllers/eventcreated.js');
+const eventDetailsControllers = require('../controllers/eventDetails.js');
 
 const router = new Router();
 router.get('/', indexControllers.index);
