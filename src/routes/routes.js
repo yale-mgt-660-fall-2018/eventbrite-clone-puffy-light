572847310538e20a5a352e9092655b0bbbfc8426
dangerousn <32247215+dangerousn@users.noreply.github.com>
2018-11-13 20:17:13 -0500

    Added the about page; Created the layout for all html (see layout.njk); WARNING: Included routes for eventDetails page, but no controller or njk file because we don't know what will come from Thomas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,11 +2,13 @@ const Router = require('koa-router');
 const indexControllers = require('../controllers/index.js');
 const neweventControllers = require('../controllers/newevent.js');
 const eventcreatedControllers = require('../controllers/eventcreated.js');
+const eventDetailsControllers = require('../controllers/eventDetails.js');
 
 const router = new Router();
 router.get('/', indexControllers.index);
 router.get('/events/new', neweventControllers.newevent);
 router.post('/events/new', eventcreatedControllers.newevent);
-router.get('/events/:id', eventControllers.getEvent);
+router.get('/events/:id', eventDetailsControllers.eventDetails);
+router.get('/about', indexControllers.about);
 
 module.exports = router;
