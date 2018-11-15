29dc448a5ac7f449405078582830313db3323f5a
dangerousn <32247215+dangerousn@users.noreply.github.com>
2018-11-15 17:16:59 -0500

    Added event details page functionality

--- src/controllers/eventDetails.js
+++ src/controllers/eventDetails.js
@@ -1,26 +1,23 @@
 /**
-* @param  {Context} ctx - A Koa Context
-* @returns {Promise} - Returns a promise that resolves to undefined
-*/
-const events = require('../models/events.js');
+ * @param  {Context} ctx - A Koa Context
+ * @returns {Promise} - Returns a promise that resolves to undefined
+ */
+ const events = require('../models/events.js');
 
-async function eventDetails(ctx) {
-   const template = 'eventDetails.njk';
-   //const waysOfBeingAwesome = ['awesome1', 'awesome2', 'awesome3'];
-
-   let event;
-
-       try {
-         event = await events.eventDetails(ctx.db);
-       }catch(e) {
-         eventList = []
-         console.log(e);
-       }
-
-
-   return ctx.render(template, {eventList });
+ async function eventDetails(ctx) {
+    const template = 'eventDetails.njk';
+    //const waysOfBeingAwesome = ['awesome1', 'awesome2', 'awesome3'];
+    console.log(events.getEvent);
+     let event;
+         try {
+          event = await events.getEvent(ctx.db, Number(ctx.req.url.match(/[0-9]/)[0]));
+          console.log(event);
+        } catch(e) {
+          event = []
+          console.log(e);
+        }
+     return ctx.render(template, {event });
 }
-
-module.exports = {
-   eventDetails,
+ module.exports = {
+    eventDetails
 };
