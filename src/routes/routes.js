d3148a40e5e7af7183da943ab7cb209ade721504
Diggers321 <thomas.digby@yale.edu>
2018-12-05 10:55:12 -0500

    As a user, I want to access an API returning all the events in a JSON format, left space for search request as currently just executing else

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ const indexControllers = require('../controllers/index.js');
 const newEventControllers = require('../controllers/newEvent.js');
 const eventCreatedControllers = require('../controllers/eventCreated.js');
 const eventDetailsControllers = require('../controllers/eventDetails.js');
+const eventApiControllers = require('../controllers/api.js');
 const rsvpControllers = require('../controllers/rsvp.js');
 
 
@@ -10,9 +11,10 @@ const router = new Router();
 router.get('/', indexControllers.index);
 router.get('/events/new', newEventControllers.newEvent);
 router.post('/events/new', eventCreatedControllers.newEvent);
-router.get('/events/:id', eventDetailsControllers.eventDetails);
 router.get('/about', indexControllers.about);
 router.post('/rsvp/:id', rsvpControllers.attendeeRegistrationPost);
+router.get('/events/api', eventApiControllers.eventApi);
+router.get('/events/:id', eventDetailsControllers.eventDetails);
 router.post('/events/:id', eventDetailsControllers.eventDetails);
 
 module.exports = router;
